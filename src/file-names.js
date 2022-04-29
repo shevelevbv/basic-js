18ba25abcb9d5fa041c69183739f1be61c49d31c
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-04-29 16:45:14 -0400

    feat: solve File Names

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let namesObject = {};
+  let resultingArray = [];
+  names.forEach(name => {
+    if (namesObject.hasOwnProperty(name)) {
+      namesObject[name]++;
+      namesObject[`${name}(${namesObject[name]})`] = 0;
+    } else {
+      namesObject[name] = 0;
+    }
+    resultingArray.push(namesObject[name] === 0 ? name: `${name}(${namesObject[name]})`);
+  });
+  return resultingArray;
 }
 
 module.exports = {
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  if (n.length !== 17) { 
+    return false;
+  }
+  let hexPattern = '0123456789ABCDEF';
+  for (let item of n.split('-')) {
+    for (let digit of item) {
+      if (!hexPattern.includes(digit)) {
+        return false;
+      }
+    }
+  };
+  return true;
 }
+
+console.log(isMAC48Address('GP-00-00-00-00-00'));
+
+
 module.exports = {
   isMAC48Address
 };
